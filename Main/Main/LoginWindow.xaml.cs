c6691f91363218ff2df5b11324b00f4ae27e53b0
Quentin JADOUL <18155@ecam.be>
2022-02-21 14:44:20 +0100

    change new

--- Main/Main/LoginWindow.xaml.cs
+++ Main/Main/LoginWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace Main
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            ProductionWindow mnw = new ProductionWindow();
+            ProductionWindow mnw = new();
             mnw.Owner = Window.GetWindow(this);
             mnw.ShowDialog();
             this.Close();
