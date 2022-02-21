04d677bf599bdd145e8d50918a872824edda2c74
Baudouin Losseau <losseaubaudouin@hotmail.com>
2022-02-21 16:10:25 +0100

    pp-34

---
+++ Main/Main/DBConn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Main
+{
+    internal class DBConn
+    {
+        private void testDB()
+        {
+            MySqlConnection sqldb = new MySqlConnection(@"server=;user id=;pwd=;port=;database=");
+            try
+            {
+                sqldb.Open();
+
+                String sql = "SELECT * FROM invoices";
+                MySqlCommand cmd = new MySqlCommand(sql, sqldb);
+
+                MySqlDataReader myDbReader = null;
+
+                myDbReader = cmd.ExecuteReader();
+
+                Console.WriteLine(myDbReader.ToString());
+
+            }
+        }
+    }
+}
