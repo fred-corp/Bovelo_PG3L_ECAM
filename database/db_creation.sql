d6d09e23b8e1f87dbe5bfbf5cf76dd24b9c6b511
fred-corp <freddruppel@gmail.com>
2022-02-28 14:09:52 +0100

    changes DB model

--- database/db_creation.sql
+++ database/db_creation.sql
@@ -2,34 +2,45 @@
         
 CREATE TABLE Catalog
 (
-  ID          INTEGER    NOT NULL,
-  model       LINESTRING NOT NULL,
-  color       INTEGER    NOT NULL,
-  size        INTEGER    NOT NULL,
-  description LINESTRING NULL    ,
-  image       BLOB       NULL    ,
+  ID          INTEGER      NOT NULL,
+  model       VARCHAR(255) NOT NULL,
+  color       INTEGER      NOT NULL,
+  size        INTEGER      NOT NULL,
+  description VARCHAR(255) NULL    ,
+  image       BLOB         NULL    ,
   PRIMARY KEY (ID)
+);
+
+CREATE TABLE Colors
+(
+  ID          INTEGER      NOT NULL,
+  description VARCHAR(255) NULL    ,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE componentlink
+(
+  part_number INTEGER NOT NULL,
+  ID          INTEGER NOT NULL
 );
 
 CREATE TABLE Components
 (
-  part_number INTEGER    NOT NULL,
-  ID          INTEGER    NOT NULL,
-  amount      INTEGER    NULL    ,
-  in_stock    INTEGER    NULL    ,
-  description LINESTRING NULL    ,
-  location    LINESTRING NULL    ,
+  part_number INTEGER      NOT NULL,
+  in_stock    INTEGER      NULL    ,
+  description VARCHAR(255) NULL    ,
+  location    VARCHAR(255) NULL    ,
   PRIMARY KEY (part_number)
 );
 
 CREATE TABLE customers
 (
-  customer_number INTEGER    NOT NULL,
-  firstname       LINESTRING NULL    ,
-  lastname        LINESTRING NULL    ,
-  address         LINESTRING NULL    ,
-  phone           LINESTRING NULL    ,
-  email           LINESTRING NULL    ,
+  customer_number INTEGER      NOT NULL,
+  firstname       VARCHAR(255) NULL    ,
+  lastname        VARCHAR(255) NULL    ,
+  address         VARCHAR(255) NULL    ,
+  phone           VARCHAR(255) NULL    ,
+  email           VARCHAR(255) NULL    ,
   PRIMARY KEY (customer_number)
 );
 
@@ -49,11 +60,6 @@ CREATE TABLE invoices
   PRIMARY KEY (invoice_number)
 );
 
-ALTER TABLE Components
-  ADD CONSTRAINT FK_Catalog_TO_Components
-    FOREIGN KEY (ID)
-    REFERENCES Catalog (ID);
-
 ALTER TABLE invoices
   ADD CONSTRAINT FK_customers_TO_invoices
     FOREIGN KEY (customer_number)
@@ -66,6 +72,16 @@ ALTER TABLE invoice_details
 
 ALTER TABLE invoice_details
   ADD CONSTRAINT FK_Catalog_TO_invoice_details
+    FOREIGN KEY (ID)
+    REFERENCES Catalog (ID);
+
+ALTER TABLE componentlink
+  ADD CONSTRAINT FK_Components_TO_componentlink
+    FOREIGN KEY (part_number)
+    REFERENCES Components (part_number);
+
+ALTER TABLE componentlink
+  ADD CONSTRAINT FK_Catalog_TO_componentlink
     FOREIGN KEY (ID)
     REFERENCES Catalog (ID);
 
