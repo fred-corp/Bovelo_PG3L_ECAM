08025b84ab77f837dcadcb461e7dc8bc92913af6
1tnx <lancelot.neirinckx+github@gmail.com>
2022-03-25 10:53:42 +0100

    PP-34 create database connection

--- Main/Main/MWM/View/StockView.xaml.cs
+++ Main/Main/MWM/View/StockView.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Configuration;
 
 namespace Main.MWM.View
 {
@@ -20,9 +23,12 @@ namespace Main.MWM.View
     /// </summary>
     public partial class StockView : UserControl
     {
+        MySqlConnection conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+
         public StockView()
         {
             InitializeComponent();
+            FillGrid();
         }
 
         private void AddPart(object sender, RoutedEventArgs e)
@@ -33,5 +39,33 @@ namespace Main.MWM.View
         {
             addPartPopup.IsOpen = false;
         }
+        private void FillGrid()
+        {
+
+            try
+            {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM Components", conn);
+
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+
+                DataSet ds = new DataSet();
+
+                adp.Fill(ds, "LoadDataBinding");
+
+                stockDataGrid.DataContext = ds;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Failed to connect to data source " + ex).ToString();
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
     }
+
 }
