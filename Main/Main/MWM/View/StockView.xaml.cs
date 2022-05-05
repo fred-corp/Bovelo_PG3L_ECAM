ad0029f8ea748c4eccdb3c0a00dcab53d7e8b88e
1tnx <lancelot.neirinckx+github@gmail.com>
2022-05-05 21:17:57 +0200

    Update stockview

--- Main/Main/MWM/View/StockView.xaml.cs
+++ Main/Main/MWM/View/StockView.xaml.cs
@@ -1,20 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using MySql.Data.MySqlClient;
 using System.Data;
 using System.Configuration;
+using System.Globalization;
 
 namespace Main.MWM.View
 {
@@ -31,14 +22,6 @@ namespace Main.MWM.View
             FillGrid();
         }
 
-        private void AddPart(object sender, RoutedEventArgs e)
-        {
-            addPartPopup.IsOpen = true;
-        }
-        private void CancelPart(object sender, RoutedEventArgs e)
-        {
-            addPartPopup.IsOpen = false;
-        }
         private void FillGrid()
         {
 
@@ -52,7 +35,7 @@ namespace Main.MWM.View
 
                 DataSet ds = new DataSet();
 
-                adp.Fill(ds, "LoadDataBinding");
+                adp.Fill(ds, "orderDataBinding");
 
                 stockDataGrid.DataContext = ds;
             }
@@ -66,6 +49,71 @@ namespace Main.MWM.View
             }
         }
 
+        private void UseButton(object sender, RoutedEventArgs e)
+        {
+            UsePopup.IsOpen = true;
+        }
+
+        private void CancelUseButton(object sender, RoutedEventArgs e)
+        {
+            UsePopup.IsOpen = false;
+        }
+
+        private void OrderButton(object sender, RoutedEventArgs e)
+        {
+            OrderPopup.IsOpen = true;
+        }
+
+        private void CancelOrderButton(object sender, RoutedEventArgs e)
+        {
+            OrderPopup.IsOpen = false;
+        }
+
+        private void ParametersButton(object sender, RoutedEventArgs e)
+        {
+            ParametersPopup.IsOpen = true;
+        }
+
+        private void CancelParametersButton(object sender, RoutedEventArgs e)
+        {
+            ParametersPopup.IsOpen = false;
+        }
+
+        private void AddPartButton(object sender, RoutedEventArgs e)
+        {
+            AddPartPopup.IsOpen = true;
+        }
+
+        private void CancelAddPartButton(object sender, RoutedEventArgs e)
+        {
+            AddPartPopup.IsOpen = false;
+        }
     }
 
+    public class PartColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int amount = System.Convert.ToInt32(value);
+            int minimumAmount = System.Convert.ToInt32(parameter);
+            if (amount - minimumAmount > 10)
+            {
+                return "Green";
+            }
+
+            else if (amount - minimumAmount > 5)
+            {
+                return "Yellow";
+            }
+            else
+            {
+                return "Red";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
