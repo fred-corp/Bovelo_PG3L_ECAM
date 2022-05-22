971d0308e79bb47b58f6d7818ce066132262b3fe
1tnx <73384301+1tnx@users.noreply.github.com>
2022-05-22 22:07:36 +0200

    PP-44: wip production management

--- Main/Main/MWM/View/ProductionView.xaml.cs
+++ Main/Main/MWM/View/ProductionView.xaml.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
 
 namespace Main.MWM.View
 {
@@ -21,169 +11,82 @@ namespace Main.MWM.View
     /// </summary>
     public partial class ProductionView : UserControl
     {
-        List<string> _Available = new List<string>();
-        Grid grid = new Grid();
-        DatePicker Picker = new DatePicker();
-        Button Planning = new Button();
-        Button Hide = new Button();
-        Button AddCommand = new Button();
-        Button DisplayStock = new Button();
-        TextBox textBox = new TextBox();
-        public string Text { get; set; }
-
-
+        private MySqlDataAdapter adp = new MySqlDataAdapter();
+        private MySqlCommandBuilder cmbl;
+        readonly MySqlConnection connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
         public ProductionView()
         {
-            
-
             InitializeComponent();
-            _Available.Add("Yes");
-            _Available.Add("NO");
-            TabItem tab = new TabItem();
-            tab.Content = GetGrid();
-            MainTabControl.Items.Add(tab);
-
-
-            grid.Name = "GridProduction"; ;
-
-            ColumnDefinition Column1 = new ColumnDefinition();
-            ColumnDefinition Column2 = new ColumnDefinition();
-            ColumnDefinition Column3 = new ColumnDefinition();
-            ColumnDefinition Column4 = new ColumnDefinition();
-
-            RowDefinition Row1 = new RowDefinition();
-            RowDefinition Row2 = new RowDefinition();
-            RowDefinition Row3 = new RowDefinition();
-            RowDefinition Row4 = new RowDefinition();
-
-            grid.ColumnDefinitions.Add(Column1);
-            grid.ColumnDefinitions.Add(Column2);
-            grid.ColumnDefinitions.Add(Column3);
-            //grid.ColumnDefinitions.Add(Column4);
-
-            grid.RowDefinitions.Add(Row1);
-            grid.RowDefinitions.Add(Row2);
-            grid.RowDefinitions.Add(Row3);
-            //grid.RowDefinitions.Add(Row4);
-
-            
-
-            ///Searching input 
-            //textBox.TextChanged += GetText;
-            //textBox.KeyDown += GetText;
-
-            Grid.SetRow(textBox, 0);
-            Grid.SetColumn(textBox, 0);
-            grid.Children.Add(textBox);
-
-            ///DisplayStock Button
-            Grid.SetColumn(DisplayStock, 1);
-            Grid.SetRow(DisplayStock, 0);
-            DisplayStock.Content = "DisplayStock";
-            DisplayStock.Name = "DisplayStock";
-            //DisplayStock.Click += Displaystock;
-            //grid.Children.Add(DisplayStock);
-
-            //Planning button
-            Grid.SetColumn(Planning, 2);
-            Grid.SetRow(Planning, 0);
-            Planning.Content = "Calendar ";
-            Planning.Name = "Planning";
-            Planning.Click += DisplayPlanning;
-            grid.Children.Add(Planning);
-
-            //ADD COMMAND 
-            Grid.SetColumn(AddCommand, 1);
-            Grid.SetRow(AddCommand, 1);
-            AddCommand.Content = "AddCommand";
-            AddCommand.Name = "AddCommand";
-            AddCommand.Click += AddComand;
-            grid.Children.Add(AddCommand);
-
-
-            
         }
 
-        public Grid GetGrid()
+        private void ShowScheduleButton(object sender, RoutedEventArgs e)
         {
-     
-            return grid;
-
+            MenuStackPanel.Visibility = Visibility.Collapsed;
+            ScheduleStackPanel.Visibility = Visibility.Visible;
         }
 
-        public void DisplayPlanning(object sender, RoutedEventArgs e)
-            //
+        private void ShowInvoicesButton(object sender, RoutedEventArgs e)
         {
-            //Remove planning button
-            grid.Children.Remove(Planning);
-
-            //DatePicker
-            Picker.Name = "Picker";
-            String Text = Picker.Text;
-            //Picker.SelectedDateChanged += ShowDate;
-            //Console.WriteLine(Text);
-            grid.Children.Add(Picker);
-            Grid.SetColumn(Picker, 2);
-            Grid.SetRow(Picker, 0);
-
-            
-            ///Hide Calendar
-            Grid.SetColumn(Hide, 2);
-            Grid.SetRow(Hide, 1);
-            Hide.Content = "Hide Calendar";
-            Hide.Name = "Hide";
-            Hide.Click += HideCalendar;
-            grid.Children.Add(Hide);
-            
+            MenuStackPanel.Visibility = Visibility.Collapsed;
+            FillInvoicesGrid();
+            InvoicesStackPanel.Visibility = Visibility.Visible;
         }
-        
-        private void HideCalendar(object sender, RoutedEventArgs e)
-        {
-            
-            grid.Children.Remove(Picker);
-            //Planning button
-            Grid.SetColumn(Planning, 2);
-            Grid.SetRow(Planning, 0);
-            grid.Children.Add(Planning);
-            Console.WriteLine("Hide");
-            grid.Children.Remove(Hide);
 
-        }
-        /*
-        private void ShowDate(object sender, RoutedEventArgs e)
+        private void ShowProductionButton(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine(sender.ToString());  //Date selectionnée dans la calendrier 
+            MenuStackPanel.Visibility = Visibility.Collapsed;
+            FillProductionGrid();
+            ProductionStackPanel.Visibility = Visibility.Visible;
         }
-        
-        private void GetText(object sender, KeyEventArgs e)
+
+        private void BackButton(object sender, RoutedEventArgs e)
         {
-            if (e.Key==Key.Enter) 
+            ScheduleStackPanel.Visibility = Visibility.Collapsed;
+            InvoicesStackPanel.Visibility = Visibility.Collapsed;
+            ProductionStackPanel.Visibility = Visibility.Collapsed;
+            MenuStackPanel.Visibility = Visibility.Visible;
+        }
+
+        private void FillInvoicesGrid()
+        {
+            try
             {
-                Console.WriteLine(sender.ToString());
-
+                connection.Open();
+                adp.SelectCommand = new MySqlCommand(@"SELECT * FROM invoices", connection);
+                cmbl = new MySqlCommandBuilder(adp);
+                DataSet ds = new DataSet();
+                adp.Fill(ds, "invoicesDataBinding");
+                InvoicesDataGrid.DataContext = ds;
             }
-            
-
-        }
-        private void Displaystock(object sender, RoutedEventArgs e)
-        {
-            
-            Console.WriteLine("DisplayStock");
-        }*/
-        private void  AddComand(object sender, RoutedEventArgs e)
-        {
-            Console.WriteLine(Picker.Text);
-            Console.WriteLine(textBox.Text);
-
-
-            Console.WriteLine("Add Command");
-            
-
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Failed to connect to data source " + ex).ToString();
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
 
-        private void MainTabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void FillProductionGrid()
         {
-
+            try
+            {
+                connection.Open();
+                adp.SelectCommand = new MySqlCommand(@"SELECT * FROM production", connection);
+                cmbl = new MySqlCommandBuilder(adp);
+                DataSet ds = new DataSet();
+                adp.Fill(ds, "productionDataBinding");
+                ProductionDataGrid.DataContext = ds;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Failed to connect to data source " + ex).ToString();
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
